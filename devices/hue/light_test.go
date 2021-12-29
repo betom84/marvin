8ebb33d39eabd60375fb04ce67d2e3bed839b50f
Tom Beyer <tom@betom.de>
2021-12-29 20:01:27 +0100

    Collect hue device operation metrics

--- devices/hue/light_test.go
+++ devices/hue/light_test.go
@@ -3,47 +3,52 @@ package hue_test
 import (
 	"marvin/config"
 	"marvin/devices/hue"
+	"marvin/metrics"
 	"testing"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	promtest "github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/assert"
 )
 
-// todo
-func _TestLight(t *testing.T) {
+func TestLight(t *testing.T) {
 	tt := []struct {
-		name  string
-		light hue.Light
+		name    string
+		light   hue.Light
+		metrics map[string]int
 	}{
 		{
 			name:  "Change state of light 3",
 			light: hue.NewLight(3, config.Get().PhilipsHueHost, config.Get().PhilipsHueUser),
+			metrics: map[string]int{
+				"application_device_operation_duration_milliseconds": 2,
+			},
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
+			prom := prometheus.NewRegistry()
+			metrics.Instance().Register(prom)
+
 			originState, err := tc.light.State()
-			if err != nil {
-				t.Fatalf("got an error; %v", err)
-			}
+			assert.NoError(t, err)
 
 			changedState, err := tc.light.SetState(!originState)
-			if err != nil {
-				t.Fatalf("got an error; %v", err)
-			}
+			assert.NoError(t, err)
+			assert.NotEqual(t, originState, changedState)
 
-			if changedState == originState {
-				t.Fatalf("state was not changed to %v", !originState)
-			}
-
-			time.Sleep(1 * time.Second)
+			time.Sleep(2 * time.Second)
 
 			changedState, err = tc.light.SetState(originState)
-			if err != nil {
-				t.Fatalf("got an error; %v", err)
-			}
+			assert.NoError(t, err)
+			assert.Equal(t, originState, changedState)
 
-			if changedState != originState {
-				t.Fatalf("state was not changed back to origin %v", originState)
+			for em, ec := range tc.metrics {
+				c, err := promtest.GatherAndCount(prom, em)
+				assert.NoError(t, err)
+				assert.Equal(t, ec, c)
 			}
 		})
 	}
